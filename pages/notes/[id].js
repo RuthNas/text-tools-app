67206b09bc57d7bd161fe55560e42870e08e9acd
Ruth Nassar <ruth.nassar@bbc.co.uk>
2022-05-13 17:12:09 +0100

    Trying route parameters

--- pages/notes/[id].js
+++ pages/notes/[id].js
@@ -1,5 +1,14 @@
 import React from 'react'
+import { useRouter } from 'next/router'
 
-const Page = () => <div>Note by id</div>
+const Page = () => {
+  const router = useRouter()
+
+  const { id } = router.query
+
+  return (
+    <h1>Has the id {id}</h1>
+  )
+}
 
 export default Page
