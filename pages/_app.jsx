f13b21ba5a7802f91d6d15180b7ec22a2418b629
Ruth Nassar <ruth.nassar@bbc.co.uk>
2022-05-23 14:12:25 +0100

    More styling with theme ui

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,11 +1,16 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx } from 'theme-ui'
 import React from 'react'
-import {ThemeProvider} from 'theme-ui'
+import { ThemeProvider } from 'theme-ui'
 import theme from '../theme'
+import Nav from '../src/components/nav'
 
 export default function App({ Component, pageProps }) {
   return (
   <ThemeProvider theme={theme} >
     <Component{...pageProps}/>
+    <Nav/>
   </ThemeProvider>
   )
 }
