dd58194a6511f3833f7faad769e2270e92252590
Ruth Nassar <ruth.nassar@bbc.co.uk>
2022-06-22 09:38:54 +0100

    Add handler for get and post requests

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,7 +1,11 @@
 import nc from 'next-connect';
 
-export default (req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify({message: "Hello"}));
-}
+const handler = nc()
+  .get((req, res)  => {
+    res.json({message: 'ok'})
+  })
+  .post((req, res) => {
+    res.json({message: 'posted'})
+  });
+  
+export default handler;
